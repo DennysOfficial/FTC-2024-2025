8d7118924a9ad927e4946479529d427e1b4eb181
DennysOfficial <143767880+DennysOfficial@users.noreply.github.com>
2024-09-12 22:35:59 -0400

    added some new config stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/profiles_and_base_settings/ModifiedConfig.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.profiles_and_base_settings;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class ModifiedConfig extends RobotConfig {
+
+    public static final float driveSensitivity = 2;
+
+    public ModifiedConfig(LinearOpMode opModeTemp) {
+        super(opModeTemp);
+    }
+}
