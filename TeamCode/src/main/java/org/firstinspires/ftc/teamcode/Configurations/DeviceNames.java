0a9b8dabe630bb14298b9818887174955f3ed509
yahaha5788 <jt.yahaha5788@gmail.com>
2024-09-21 15:31:00 -0400

    THE EYE OF WISDOM RETURNS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configurations/DeviceNames.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configurations/DeviceNames.java
@@ -4,53 +4,26 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class DeviceNames {
-    public static String frontRightDrive = "FR";
 
-    public String getFrontRightDrive() {
-        return frontRightDrive;
-    }
+    //gaze into the eye of wisdom and all your code will work
 
-    public static String frontLeftDrive = "FL";
+                      public static String frontRightDrive = "FR";
+              public String getFrontRightDrive() {return frontRightDrive;}
+         public    static                              String frontLeftDrive = "FL";
+       public    String                                   getFrontLeftDrive() {return frontRightDrive;}
+      public    static               /**/                  String backRightDrive = "BR";
+     public   String                 /**/                  getBackRightDrive() {return backRightDrive;}
+    public static                   /*[]*/                  String backLeftDrive = "BL";
+    public String                   /*[]*/                  getBackLeftDrive() {return backLeftDrive;}
+     public   static                 /**/                  String leftPivot = "PivotL";
+      public    String               /**/                  getLeftPivot() {return leftPivot;}
+       public     static                               String rightPivot = "PivotR";
+               public String getRightPivot() {    return     rightPivot;}
+                     public  static  String  leftLift  =  "LiftL";
 
-    public String getFrontLeftDrive() {
-        return frontRightDrive;
-    }
 
-    public static String backRightDrive = "BR";
-
-    public String getBackRightDrive() {
-        return backRightDrive;
-    }
-
-    public static String backLeftDrive = "BL";
-
-    public String getBackLeftDrive() {
-        return backLeftDrive;
-    }
-
-    public static String leftPivot = "PivotL";
-
-    public String getLeftPivot() {
-        return leftPivot;
-    }
-
-    public static String rightPivot = "PivotR";
-
-    public String getRightPivot() {
-        return rightPivot;
-    }
-
-    public static String leftLift = "LiftL";
-
-    public String getLeftLift() {
-        return leftLift;
-    }
-
+    public String getLeftLift() {return leftLift;}
     public static String rightLift = "LiftR";
-
-    public String getRightLift() {
-        return rightLift;
-    }
-
+    public String getRightLift() {return rightLift;}
 
 }
