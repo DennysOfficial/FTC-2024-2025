83e55e310f5e69013d64b1cf99e97c760ace32c1
yahaha5788 <jt.yahaha5788@gmail.com>
2024-09-21 15:32:25 -0400

    THE EYE OF WISDOM RETURNS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configurations/DeviceNames.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configurations/DeviceNames.java
@@ -10,13 +10,13 @@ public class DeviceNames {
                       public static String frontRightDrive = "FR";
               public String getFrontRightDrive() {return frontRightDrive;}
          public    static                              String frontLeftDrive = "FL";
-       public    String                                   getFrontLeftDrive() {return frontRightDrive;}
+       public    String                                  getFrontLeftDrive() {return frontRightDrive;}
       public    static               /**/                  String backRightDrive = "BR";
      public   String                 /**/                  getBackRightDrive() {return backRightDrive;}
-    public static                   /*[]*/                  String backLeftDrive = "BL";
-    public String                   /*[]*/                  getBackLeftDrive() {return backLeftDrive;}
+    public  static                  /*[]*/                   String backLeftDrive = "BL";
+    public  String                  /*[]*/                   getBackLeftDrive() {return backLeftDrive;}
      public   static                 /**/                  String leftPivot = "PivotL";
-      public    String               /**/                  getLeftPivot() {return leftPivot;}
+      public    String               /**/                getLeftPivot() {return leftPivot;}
        public     static                               String rightPivot = "PivotR";
                public String getRightPivot() {    return     rightPivot;}
                      public  static  String  leftLift  =  "LiftL";
