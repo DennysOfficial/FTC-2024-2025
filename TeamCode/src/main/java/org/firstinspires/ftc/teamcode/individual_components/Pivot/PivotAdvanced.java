ae64446d01ceef3723deb846e429852c2f5ab5d7
DennysOfficial <jackmgibel@gmail.com>
2024-10-05 00:02:44 -0400

    eeeeeeeeeeeeeeeeeeee

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/individual_components/Pivot/PivotAdvanced.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/individual_components/Pivot/PivotAdvanced.java
@@ -2,8 +2,6 @@ package org.firstinspires.ftc.teamcode.individual_components.Pivot;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.MotorControlAlgorithm;
-import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import org.firstinspires.ftc.teamcode.Configurations.RobotConfig;
@@ -11,14 +9,30 @@ import org.firstinspires.ftc.teamcode.Configurations.RobotConfig;
 @Config
 public class PivotAdvanced extends PivotBasic {
 
-    public static double liftCGRadiusRetracted = 5;
-    public static double liftMovingMassProportion = 0.35f;
-    public static double liftMass = 0.004;
-    public static double staticFrictionTorque = 0;
-    public static double kinematicFrictionTorque = 0;
+
+
     public static double staticThresholdDPS = 10;
-    public static double maxMotorSpeedRPM = 6000;
-    public static double maxMotorAmps;
+
+    @Config
+    public static class coefficients{
+        public static double staticFrictionTorque = 0;
+        public static double kinematicFrictionTorque = 0;
+        public static double gravityCoefficient = 1;
+        public static double directControlDamping = 0;
+    }
+
+    @Config
+    public static class liftProperties {
+        public static double CGRadiusRetracted = 5;
+        public static double movingMassProportion = 0.35f;
+        public static double mass = 0.004;
+    }
+
+    @Config
+    public static class motorProperties {
+        public static double maxSpeedRPM = 6000;
+        public static double stallAmps;
+    }
 
     public static boolean advancedDebugEnabled = false;
 
@@ -32,7 +46,7 @@ public class PivotAdvanced extends PivotBasic {
     }
 
     public void directControlFancy(double liftExtension) {
-        setTorqueFeedforwardCompensated(config.getPivotStick(),liftExtension);
+        setTorque(calculateNetTorque(config.getPivotStick(), liftExtension) - calculateDragTorque(getVelocityDPS(),coefficients.directControlDamping));
     }
 
     @Override
@@ -41,33 +55,33 @@ public class PivotAdvanced extends PivotBasic {
     }
 
     public void setTorque(double targetTorque) {
-        double motorSpeedRPM = getVelocityTPS()/encoderCountsPerRevMotor;
-        double motorPower = targetTorque + motorSpeedRPM/maxMotorSpeedRPM;
+        double motorSpeedRPM = getVelocityTPS() / encoderCountsPerRevMotor;
+        double motorPower = targetTorque + motorSpeedRPM / motorProperties.maxSpeedRPM;
 
-        if(advancedDebugEnabled){
+        if (advancedDebugEnabled) {
             opMode.telemetry.addData("targetTorque", targetTorque);
-            opMode.telemetry.addData("motorCurrent",getCurrentAmp());
+            opMode.telemetry.addData("motorCurrent", getCurrentAmp());
         }
 
         setPower(motorPower);
     }
 
-    public void setTorqueFeedforwardCompensated(double targetPower, double liftExtension) {
-        double outputTorque = targetPower;
-        double gravityTorque = calculateTorqueGravity(liftCGRadiusRetracted + liftExtension * liftMovingMassProportion, getAngle(), liftMass);
-        double frictionTorque = calculateTorqueFriction(targetPower, getVelocityDPS(), staticFrictionTorque, kinematicFrictionTorque, staticThresholdDPS);
-        outputTorque -= gravityTorque;
-        outputTorque -= frictionTorque;
-        setTorque(outputTorque);
+    public double calculateNetTorque(double targetNetTorque, double liftExtension) {
 
-        if (advancedDebugEnabled){
+        double gravityTorque = calculateTorqueGravity(liftProperties.CGRadiusRetracted + liftExtension * liftProperties.movingMassProportion, getAngle(), liftProperties.mass);
+        double frictionTorque = calculateTorqueFriction(targetNetTorque, getVelocityDPS(), coefficients.staticFrictionTorque, coefficients.kinematicFrictionTorque, staticThresholdDPS);
+
+        double outputTorque = targetNetTorque - gravityTorque -frictionTorque;
+
+        if (advancedDebugEnabled) {
             opMode.telemetry.addData("gravityTorque", gravityTorque);
-            opMode.telemetry.addData("frictionTorque",frictionTorque);
+            opMode.telemetry.addData("frictionTorque", frictionTorque);
         }
+        return outputTorque;
     }
 
     double calculateTorqueGravity(double liftCGRadius, double pivotAngleDeg, double liftMass) {
-        return liftCGRadius * Math.sin(Math.toRadians(pivotAngleDeg)) * liftMass;
+        return liftCGRadius * Math.sin(Math.toRadians(pivotAngleDeg)) * liftMass * coefficients.gravityCoefficient;
     }
 
     double calculateTorqueFriction(double targetDirection, double pivotVelocity, double staticFrictionTorque, double kinematicFrictionTorque, double staticThreshold) {
@@ -81,11 +95,15 @@ public class PivotAdvanced extends PivotBasic {
         return liftCGRadius * liftCGRadius * targetAcceleration;
     }
 
+    double calculateDragTorque(double pivotVelocity, double dragCoefficient) {
+        return pivotVelocity * dragCoefficient;
+    }
+
     /**
      * @return average current between the two motors in amps
      */
-    double getCurrentAmp(){
-       return (pivotMotorL.getCurrent(CurrentUnit.AMPS) + pivotMotorR.getCurrent(CurrentUnit.AMPS))/2.0;
+    double getCurrentAmp() {
+        return (pivotMotorL.getCurrent(CurrentUnit.AMPS) + pivotMotorR.getCurrent(CurrentUnit.AMPS)) / 2.0;
     }
 
     public void updatePID(double deltaTime) {
