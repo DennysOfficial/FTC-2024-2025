75c36c282db1d00632655d9c0e7db806dae26086
Nucleus (Anders) <aksandvick@hotmail.com>
2024-11-02 11:06:07 -0400

    Merged Automous code with TeleOp code, Added simple auto routine

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Automous_Async_Simple.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Config
+@Autonomous(name = "soupcOpMode_Simple")
+public class Automous_Async_Simple extends LinearOpMode {
+
+    Trajectory toObservationZone;
+    Pose2d startPose = new Pose2d(new Vector2d(60,24), Math.toRadians(180));
+
+    @Override
+    public void runOpMode() {
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        drive.setPoseEstimate(startPose);
+
+        toObservationZone = drive.trajectoryBuilder(startPose)
+                .lineToConstantHeading(new Vector2d(60,60))
+                .build();
+
+        while (!isStopRequested() && !opModeIsActive()) {
+
+        }
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        drive.followTrajectoryAsync(toObservationZone);
+
+        while (!isStopRequested() && opModeIsActive()) {
+
+            drive.update();
+
+            // Put your PID Update Function Here
+        }
+    }
+}
