be4c6728f4687d0c2f73b64ebd5439e9e058a63e
DennysOfficial <143767880+DennysOfficial@users.noreply.github.com>
2024-11-13 12:14:07 -0500

    new meep meep work

---
+++ NewMeepMeep/src/main/java/com/example/newmeepmeep/NewMeepMeep.java
@@ -0,0 +1,29 @@
+package com.example.newmeepmeep;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class NewMeepMeep {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        Pose2d initialPose = new Pose2d(0, -72+18f/2, Math.toRadians(90));
+
+        myBot.runAction(myBot.getDrive().actionBuilder(initialPose)
+                .lineToY(-31)
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
