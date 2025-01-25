1fa9b8ead51a944e3f9dd77f1b5bc84bbdbbd47e
DennysOfficial <143767880+DennysOfficial@users.noreply.github.com>
2025-01-25 10:17:33 -0500

    oga booga hit robot with rocks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/individual_components/LeftLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/individual_components/LeftLift.java
@@ -95,9 +95,9 @@ public class LeftLift extends ControlAxis {
 
 
     public LeftLift(ControlMode defaultControlMode, OpMode opMode, RobotConfig config) {
-        super(defaultControlMode, opMode, config, "LeftLift", "inches", (19.25-55)/(-44-2560));
+        super(defaultControlMode, opMode, config, "LeftLift", "inches", 1);
 
-        softLimits = new Range<>(0.5, 34.69);
+        //softLimits = new Range<>(0.5, 34.69);
 
         physicalLimits = new Range<>(0.0, Double.POSITIVE_INFINITY);
     }
