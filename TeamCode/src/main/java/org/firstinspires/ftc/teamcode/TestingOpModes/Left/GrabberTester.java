21a45572eff1e9862810cc4ce3970c5c7e78188e
Nucleus (Anders) <aksandvick@hotmail.com>
2025-01-26 15:12:12 -0500

    Organized GrabberTester

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModes/Left/GrabberTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModes/Left/GrabberTester.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.TestingOpModes;
+package org.firstinspires.ftc.teamcode.TestingOpModes.Left;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
