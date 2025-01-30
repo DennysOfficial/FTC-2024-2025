6f091f82e7fa3ff57e284dee6bb59de9f730270a
DennysOfficial <143767880+DennysOfficial@users.noreply.github.com>
2025-01-30 15:03:54 -0500

    dasfasda

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModes/HomingClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModes/HomingClass.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.TestingOpModes;
 
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
 public class HomingClass {
 
     public static double homingPower = 1;
