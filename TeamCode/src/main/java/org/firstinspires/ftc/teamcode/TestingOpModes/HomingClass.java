d73833518277f4c8b6e58d4ef003a54073a36338
DennysOfficial <143767880+DennysOfficial@users.noreply.github.com>
2025-01-30 14:42:09 -0500

    dasfasda

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModes/HomingClass.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.TestingOpModes;
+
+public class HomingClass {
+
+    public static double homingPower = 1;
+
+    public static double setCurrentAlertValue = 1;
+
+
+
+}
