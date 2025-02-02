de4062c41620829f3e88b096a580b43db72c3845
Nucleus (Anders) <aksandvick@hotmail.com>
2025-02-02 17:56:45 -0500

    Added no lift 05Z

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModes/Auto_Test_05Z_noLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModes/Auto_Test_05Z_noLift.java
@@ -15,12 +15,8 @@ import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.LeftPivot
 import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.RightLift;
 import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.RightPivot;
 import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.grabbers.ActiveIntakeMotor;
-import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.grabbers.ActiveIntakeServo;
 import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.grabbers.PassiveGrabber;
 import org.firstinspires.ftc.teamcode.pedroPathing.Automous;
-import org.firstinspires.ftc.teamcode.pedroPathing.AutomousNoLift;
-import org.firstinspires.ftc.teamcode.pedroPathing.LiftTimeStamp;
-import org.firstinspires.ftc.teamcode.pedroPathing.TimeStamp;
 import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
 import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierLine;
@@ -33,8 +29,8 @@ import org.firstinspires.ftc.teamcode.pedroPathing.util.Timer;
 
 import java.util.List;
 
-@Autonomous(name = "SoupcOpMode_0-5-Z 2.0.0")
-public class Auto_Test_05Z extends OpMode{
+@Autonomous(name = "SoupcOpMode_0-5-Z 2.0.0 Test")
+public class Auto_Test_05Z_noLift extends OpMode{
 
     List<LynxModule> allHubs;
 
@@ -69,14 +65,6 @@ public class Auto_Test_05Z extends OpMode{
 
     double deltaTime;
 
-    LeftLift leftLift;
-    LeftPivot leftPivot;
-    ActiveIntakeMotor intake;
-
-    RightLift rightLift;
-    RightPivot rightPivot;
-    PassiveGrabber grabber;
-
     RobotConfig config;
 
     Automous automous; //CHANGE THIS WHEN NEW ROBOT
@@ -84,9 +72,6 @@ public class Auto_Test_05Z extends OpMode{
     int listPointer = 1;
 
     private Timer pathTimer;
-
-    Runnable r1, r2;
-    SingleRunAction a1, a2;
 
     @Override
     public void init() {
@@ -104,26 +89,8 @@ public class Auto_Test_05Z extends OpMode{
 
         config = new RobotConfig(this);
 
-        leftLift = new LeftLift(ControlAxis.ControlMode.positionControl,this, config);
-        leftPivot = new LeftPivot(ControlAxis.ControlMode.positionControl,this, config);
-        intake = new ActiveIntakeMotor(this, config);
-
-        rightLift = new RightLift(ControlAxis.ControlMode.positionControl,this, config);
-        rightPivot = new RightPivot(ControlAxis.ControlMode.positionControl,this, config);
-        grabber = new PassiveGrabber(this, config, leftLift, leftPivot);
-
-        leftLift.assignPivot(leftPivot);
-        leftPivot.assignLift(leftLift);
-
-        rightLift.assignPivot(rightPivot);
-        rightPivot.assignLift(rightLift);
-
         follower = new Follower(hardwareMap);
         follower.setStartingPose(startPose);
-
-
-
-        automous = new Automous(this, leftLift, leftPivot, rightLift, rightPivot, intake, grabber, config, follower);
     }
 
 
@@ -131,109 +98,45 @@ public class Auto_Test_05Z extends OpMode{
     public void buildPaths() {
         movement1 = follower.pathBuilder()
                 .addPath(new Path(new BezierLine(new Point(startPose), rungPoint1)))
-                .addTemporalCallback(0.5, () -> {
-                    grabber.Score();
-                })
                 .addPath(new Path(new BezierLine(rungPoint1, samplePoint1)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
-                .addTemporalCallback(1, () -> {
-                    grabber.Rest();
-                })
-                .addTemporalCallback(2, () -> {
-                    rightPivot.setTargetPosition(90);
-                    rightLift.setTargetPosition(liftPosSpike1);
-                })
                 .build();
         movement2 = follower.pathBuilder()
-                .addPath(new Path(new BezierLine(samplePoint1,samplePoint2)))
+                .addPath(new Path(new BezierLine(samplePoint1, samplePoint2)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
-                .addTemporalCallback(0.5, () -> {
-                    rightPivot.setTargetPosition(90);
-                    rightLift.setTargetPosition(liftPosSpike1);
-                })
                 .build();
         movement3 = follower.pathBuilder()
                 .addPath(new Path(new BezierPoint(samplePoint2)))
                 .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(340))
-                .addTemporalCallback(0.5, () -> {
-                    rightPivot.setTargetPosition(90);
-                    rightLift.setTargetPosition(liftPosSpike3);
-                })
                 .build();
         movement4 = follower.pathBuilder()
                 .addPath(new Path(new BezierPoint(samplePoint2)))
                 .setLinearHeadingInterpolation(Math.toRadians(340), Math.toRadians(0))
-                .addTemporalCallback(0, () -> {
-                    grabber.Collect();
-                })
                 .addPath(new Path(new BezierLine(samplePoint2, pickupPoint1)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
                 .addPath(new Path(new BezierLine(pickupPoint1, rungPoint2)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
-                .addTemporalCallback(0.5, ()-> {
-                    grabber.Score();
-                })
                 .addPath(new Path(new BezierLine(rungPoint2, pickupPoint2)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
-                .addTemporalCallback(0.5, () -> {
-                    grabber.Collect();
-                })
                 .addPath(new Path(new BezierLine(pickupPoint2, pickupPoint3)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
                 .addPath(new Path(new BezierLine(pickupPoint2, rungPoint3)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
-                .addTemporalCallback(0.5, ()-> {
-                    grabber.Score();
-                })
                 .addPath(new Path(new BezierLine(rungPoint3, pickupPoint2)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
-                .addTemporalCallback(0.5, () -> {
-                    grabber.Collect();
-                })
                 .addPath(new Path(new BezierLine(pickupPoint2, pickupPoint3)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
                 .addPath(new Path(new BezierLine(pickupPoint2, rungPoint4)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
-                .addTemporalCallback(0.5, ()-> {
-                    grabber.Score();
-                })
                 .addPath(new Path(new BezierLine(rungPoint4, pickupPoint2)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
-                .addTemporalCallback(0.5, () -> {
-                    grabber.Collect();
-                })
                 .addPath(new Path(new BezierLine(pickupPoint2, pickupPoint3)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
                 .addPath(new Path(new BezierLine(pickupPoint2, rungPoint5)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
-                .addTemporalCallback(0.5, ()-> {
-                    grabber.Score();
-                })
                 .addPath(new Path(new BezierLine(rungPoint5, parkPoint)))
                 .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(240))
-                .addTemporalCallback(0.5, () -> {
-                    grabber.Collect();
-                })
-                .addTemporalCallback(1, () -> {
-                    rightPivot.setTargetPosition(90);
-                    rightLift.setTargetPosition(0);
-                    intake.moveWrist(0.2);
-                })
                 .build();
-
-        r1 = () -> {
-            rightLift.setTargetPosition(0);
-            rightPivot.setTargetPosition(-69);
-            intake.moveWrist(0.85);
-        };
-
-        a1 = new SingleRunAction(r1);
-
-        r2 = () -> {
-            intake.intakeForDuration(1);
-        };
-
-        a2 = new SingleRunAction(r2);
     }
 
     public void routine() {
@@ -243,27 +146,15 @@ public class Auto_Test_05Z extends OpMode{
                 if (!follower.isBusy() || pathTimer.getElapsedTimeSeconds() >= 10) {
                     listPointer = 2;
                     pathTimer.resetTimer();
-                    intake.moveWrist(0.2);
-                    intake.intakeForDuration(1);
                 }
 
                 break;
             case 2:
 
-                if (pathTimer.getElapsedTimeSeconds() >= 1) {
-                    a1.run();
-                }
-
-                if (pathTimer.getElapsedTimeSeconds() >= 2) {
-                    a2.run();
-                }
-
                 if (!follower.isBusy() || pathTimer.getElapsedTimeSeconds() >= 3) {
                     listPointer = 3;
                     follower.followPath(movement2);
                     pathTimer.resetTimer();
-                    a1.reset();
-                    a2.reset();
                 }
 
                 break;
@@ -272,27 +163,15 @@ public class Auto_Test_05Z extends OpMode{
                 if (!follower.isBusy() || pathTimer.getElapsedTimeSeconds() >= 10) {
                     listPointer = 4;
                     pathTimer.resetTimer();
-                    intake.moveWrist(0.2);
-                    intake.intakeForDuration(1);
                 }
 
                 break;
             case 4:
 
-                if (pathTimer.getElapsedTimeSeconds() >= 1) {
-                    a1.run();
-                }
-
-                if (pathTimer.getElapsedTimeSeconds() >= 2) {
-                    a2.run();
-                }
-
                 if (!follower.isBusy() || pathTimer.getElapsedTimeSeconds() >= 3) {
                     listPointer = 5;
                     follower.followPath(movement3);
                     pathTimer.resetTimer();
-                    a1.reset();
-                    a2.reset();
                 }
 
                 break;
@@ -301,27 +180,15 @@ public class Auto_Test_05Z extends OpMode{
                 if (!follower.isBusy() || pathTimer.getElapsedTimeSeconds() >= 10) {
                     listPointer = 6;
                     pathTimer.resetTimer();
-                    intake.moveWrist(0.2);
-                    intake.intakeForDuration(1);
                 }
 
                 break;
             case 6:
 
-                if (pathTimer.getElapsedTimeSeconds() >= 1) {
-                    a1.run();
-                }
-
-                if (pathTimer.getElapsedTimeSeconds() >= 2) {
-                    a2.run();
-                }
-
                 if (!follower.isBusy() || pathTimer.getElapsedTimeSeconds() >= 3) {
                     listPointer = 7;
                     follower.followPath(movement4);
                     pathTimer.resetTimer();
-                    a1.reset();
-                    a2.reset();
                 }
                 break;
             case 7:
@@ -341,11 +208,6 @@ public class Auto_Test_05Z extends OpMode{
 
         routine();
         follower.update();
-        leftLift.update();
-        leftPivot.update();
-        rightLift.update();
-        rightPivot.update();
-        intake.update();
 
         telemetry.addData("path state", automous.getPath());
         telemetry.addData("x", follower.getPose().getX());
@@ -361,8 +223,6 @@ public class Auto_Test_05Z extends OpMode{
         buildPaths();
         follower.followPath(movement1);
         pathTimer.resetTimer();
-        rightLift.setTargetPosition(0);
-        rightPivot.setTargetPosition(-69);
 
         deltaTime = 0;
         frameTimer.reset();
