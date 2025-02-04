de2674b170770c8be466f6b43adf36822d44e63e
DennysOfficial <143767880+DennysOfficial@users.noreply.github.com>
2025-02-04 16:40:19 -0500

    drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/individual_components/DriveModes/FullPIDDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/individual_components/DriveModes/FullPIDDrive.java
@@ -2,31 +2,68 @@ package org.firstinspires.ftc.teamcode.RobotStuff.individual_components.DriveMod
 
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
 
 import org.firstinspires.ftc.teamcode.RobotStuff.Config.RobotConfig;
 import org.firstinspires.ftc.teamcode.RobotStuff.stuffAndThings.Ramp;
 
-public class FullPIDDrive extends DriveModeBase{
+public class FullPIDDrive extends DriveModeBase {
+
+    public static PIDCoefficients turnPid = new PIDCoefficients(0, 0, 0);
+    public static PIDCoefficients xPID = new PIDCoefficients(0, 0, 0);
+    public static PIDCoefficients yPID = new PIDCoefficients(0, 0, 0);
 
     double drive, strafe, turn;
     SparkFunOTOS otos;
+    SparkFunOTOS.Pose2D robotPose;
 
     Ramp xAccelerationControl;
     double xRequestedTargetVelocity;
     double xTargetVelocity;
+    double xTargetPosition;
+
     Ramp yAccelerationControl;
     double yRequestedTargetVelocity;
     double yTargetVelocity;
+    double yTargetPosition;
+
+    Ramp hAccelerationControl;
+    double hRequestedTargetVelocity;
+    double hTargetVelocity;
+    double hTargetPosition;
 
 
+    public void telemetryPose() {
+        opMode.telemetry.addData("heading %f", robotPose.h);
+        opMode.telemetry.addData("x %f", robotPose.x);
+        opMode.telemetry.addData("y %f", robotPose.y);
+    }
 
     public FullPIDDrive(OpMode opMode, RobotConfig config) {
         super(opMode, config);
+        otos = opMode.hardwareMap.get(SparkFunOTOS.class, "otos");
 
+        robotPose = otos.getPosition();
+
+        xTargetPosition = robotPose.x;
+        yTargetPosition = robotPose.y;
+
+
+    }
+
+    void updateRequestedDriveVector() {
+        xRequestedTargetVelocity = Math.cos(robotPose.h) * config.inputMap.getStrafeStick();
+        xRequestedTargetVelocity = Math.sin(robotPose.h) * config.inputMap.getForwardStick();
+
+        yRequestedTargetVelocity = Math.sin(robotPose.h) * config.inputMap.getStrafeStick();
+        yRequestedTargetVelocity = Math.cos(robotPose.h) * config.inputMap.getForwardStick();
     }
 
     @Override
     public void updateDrive(double deltaTime) {
-        setDriveVector(drive,strafe,turn);
+        robotPose = otos.getPosition();
+        setDriveVector(drive, strafe, turn);
+
+
     }
 }
