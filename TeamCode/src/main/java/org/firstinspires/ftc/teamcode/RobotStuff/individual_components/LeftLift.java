3178ba289360ba4d62fb603bade7edf22a9716c7
DennysOfficial <143767880+DennysOfficial@users.noreply.github.com>
2025-02-08 08:37:19 -0500

    homing?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/individual_components/LeftLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/individual_components/LeftLift.java
@@ -138,23 +138,26 @@ public class LeftLift extends ControlAxis {
         super.setTargetPosition(targetPosition);
     }
 
+    boolean previousButtonState = false;
     @Override
     void miscUpdate() {
 
-        return;
+
 
         //opMode.telemetry.addLine("misc update running");
 
-//        if (config.inputMap.gamepad1.left_bumper) {
-//            targetTorque = homingPower;
-//            setControlModeUnsafe(ControlMode.torqueControl);
-//        } else
-//            setControlMode(defaultControlMode);
-//
-//        if (!limitSwitch.getState()) {
-//            opMode.telemetry.addData("position offset = %f", positionOffset);
-//            setCurrentPosition(homingPosition);
-//        }
+        if (config.inputMap.gamepad1.y) {
+            targetTorque = homingPower;
+            setControlModeUnsafe(ControlMode.torqueControl);
+        } else
+            setControlMode(defaultControlMode);
+
+        if(previousButtonState && !config.inputMap.gamepad1.y){
+            opMode.telemetry.addData("position offset = %f", positionOffset);
+            setCurrentPosition(homingPosition);
+        }
+
+        previousButtonState = config.inputMap.gamepad1.y;
     }
 
 }
