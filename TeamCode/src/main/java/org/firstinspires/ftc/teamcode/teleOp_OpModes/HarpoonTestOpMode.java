16ede5f7a38ae4895fb7f78a78bbcf3daa68ba1e
DennysOfficial <143767880+DennysOfficial@users.noreply.github.com>
2025-02-10 13:13:42 -0500

    harpoon perhap

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp_OpModes/HarpoonTestOpMode.java
@@ -0,0 +1,148 @@
+/* Copyright (c) 2021 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.teleOp_OpModes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.RobotStuff.Config.RobotConfig;
+import org.firstinspires.ftc.teamcode.RobotStuff.HarpoonArm;
+import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.ControlAxis;
+import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.DriveModes.BasicMechanumDrive;
+import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.DriveModes.DriveModeBase;
+import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.LeftLift;
+import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.LeftPivot;
+import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.RightLift;
+import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.RightPivot;
+import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.grabbers.ActiveIntakeMotor;
+import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.grabbers.ClawAndStuff;
+import org.firstinspires.ftc.teamcode.RobotStuff.individual_components.grabbers.speedyServos;
+import org.firstinspires.ftc.teamcode.RobotStuff.stuffAndThings.StopWatch;
+
+import java.util.List;
+
+@TeleOp(name = "Harpoon go stab", group = "Linear OpMode")
+//@Disabled
+public class HarpoonTestOpMode extends LinearOpMode {
+
+
+    private final ElapsedTime frameTimer = new ElapsedTime();
+
+
+    @Override
+    public void runOpMode() {
+
+
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+        }
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry()); // does stuff for ftc dashboard idk// bulk caching and ftc telemetry
+
+        RobotConfig activeConfig = new RobotConfig(this); // selects the active setting that will be used in the rest of the code
+
+
+        DriveModeBase activeDriveMode = new BasicMechanumDrive(this, activeConfig);
+
+
+        HarpoonArm harpoonArm = new HarpoonArm(this, activeConfig);
+
+        LeftLift leftLift = new LeftLift(ControlAxis.ControlMode.positionControl, this, activeConfig);
+
+        LeftPivot otherSpinnyBit = new LeftPivot(ControlAxis.ControlMode.positionControl, this, activeConfig);
+
+        otherSpinnyBit.assignLift(leftLift);
+        leftLift.assignPivot(otherSpinnyBit);
+
+        ClawAndStuff leftArmStuff = new ClawAndStuff(this, activeConfig, leftLift, otherSpinnyBit);
+
+        ActiveIntakeMotor suck = new ActiveIntakeMotor(this, activeConfig);
+
+        speedyServos prayers = new speedyServos(this, activeConfig);
+        //ActiveIntakeServo intake = new ActiveIntakeServo(this, activeConfig);
+
+
+        waitForStart();
+        frameTimer.reset();
+        //leftArmStuff.Rest();
+
+        double deltaTime = 0;
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            activeConfig.stopWatch.reset();
+            activeConfig.stopWatch.debug = activeConfig.debugConfig.getTimeBreakdownDebug();
+
+
+            deltaTime = frameTimer.seconds(); //gets the time since the start of last frame and then resets the timer
+            telemetry.addData("deltaTime", deltaTime);
+            frameTimer.reset();
+
+            for (LynxModule hub : allHubs) {
+                hub.clearBulkCache();
+            }
+            activeConfig.sensorData.update(); // bulk caching
+
+
+            if (gamepad2.x) {
+                leftArmStuff.Score();
+            }
+            if (gamepad2.a) {
+                leftArmStuff.Collect();
+            }
+
+
+            activeConfig.stopWatch.addTimeToTelemetryAndReset(telemetry, "main loop beginning Time -------------------------------");
+
+            harpoonArm.update();
+            activeConfig.stopWatch.addTimeToTelemetryAndReset(telemetry, "total HarpoonArm update Time ----------------------------");
+
+            activeDriveMode.updateDrive(deltaTime);
+            activeConfig.stopWatch.addTimeToTelemetryAndReset(telemetry, "drive update Time ----------------------------");
+
+
+            leftLift.update();
+            otherSpinnyBit.update();
+            suck.directControl();
+            leftArmStuff.updatePincher();
+            activeConfig.stopWatch.addTimeToTelemetryAndReset(telemetry, "other stuff ----------------------------");
+
+            telemetry.update();
+        }
+    }
+
+}
