8b85dddcf775bb5190ac4a3a94a1ed218a3a3b2b
yahaha5788 <jackbuckeyes44011@gmail.com>
2025-02-25 13:36:46 -0500

    not yet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/individual_components/DriveModes/DriveSwitch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/individual_components/DriveModes/DriveSwitch.java
@@ -7,29 +7,36 @@ import org.firstinspires.ftc.teamcode.RobotStuff.Config.RobotConfig;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DriveSwitch {
+public class DriveSwitch extends DriveModeBase{
 
     private List<DriveModeBase> driveModes = new ArrayList<>();
 
     private int activeDriveIndex = 0;
     private DriveModeBase activeDrive;
-    private String activeDriveName;
 
     OpMode opmode;
     RobotConfig config;
 
     public DriveSwitch(OpMode opmode, RobotConfig config) {
-        this.opmode = opmode;
-        this.config = config;
-        DriveModeBase defaultDriveMode = driveModes.get(0);
+        super(opmode, config);
     }
     public void addDriveMode(DriveModeBase driveMode) {
         driveModes.add(driveMode);
         activeDrive = driveMode;
-
     }
 
-    public void setActiveDrive(DriveModeBase activateDriveMode) {
+    public void setActiveDrive(int modeIndex) {
+        activeDrive = driveModes.get(modeIndex);
+    }
 
+    @Override
+    public void updateDrive(double deltaTime) {
+        if (config.playerOne.driveModeUp.getRisingState()) { // if there are two drive modes, drivemodes.size = 2 and index options are 0 and 1
+            activeDriveIndex = (activeDriveIndex + 1 == driveModes.size()) ? 0 : activeDriveIndex + 1;
+        }
+
+        if (config.playerOne.driveModeDown.getRisingState()) {
+
+        }
     }
 }
