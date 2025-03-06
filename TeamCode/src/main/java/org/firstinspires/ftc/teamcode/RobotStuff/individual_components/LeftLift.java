e62b57e649856921901a26c4422a3e962a504348
DennysOfficial <143767880+DennysOfficial@users.noreply.github.com>
2025-03-06 10:58:11 -0500

    tcyivubion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/individual_components/LeftLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/individual_components/LeftLift.java
@@ -12,7 +12,6 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.RobotStuff.Config.RobotConfig;
-import org.firstinspires.ftc.teamcode.RobotStuff.stuffAndThings.ButtonEdgeDetector;
 
 @Config
 public class LeftLift extends ControlAxis {
@@ -21,9 +20,9 @@ public class LeftLift extends ControlAxis {
 
     DigitalChannel limitSwitch;
     public static double homingPosition = 0;
-    public static double homingRetractPower = -0.5;
+    public static double homingRetractPower = -0.9;
     public static double homingDwellPower = -0.2;
-    public static double homingDwellPeriod = 0.3;
+    public static double homingDwellPeriod = 0.1;
 
 
     final double retractedRadius = 10;
@@ -44,10 +43,6 @@ public class LeftLift extends ControlAxis {
     public static double kineticFrictionCoefficient = 0;
     public static double staticThreshold = 0.1;
 
-
-    @Override
-    public void homeAxis() {
-    }
 
     @Override
     double getKp() {
@@ -139,19 +134,11 @@ public class LeftLift extends ControlAxis {
         super.setTargetPosition(targetPosition);
     }
 
-    boolean previousButtonState = false;
     double minExtension = Double.POSITIVE_INFINITY;
-
-    ButtonEdgeDetector homingButton = new ButtonEdgeDetector(false);
-
     ElapsedTime homingDwellTimer;
 
     @Override
     void miscUpdate() {
-
-//        if (homingButton.getButtonDown(config.inputMap.gamepad1.y)) {
-//            homingState = HomingState.initHoming;
-//        }
 
         if (config.inputMap.gamepad1.y) {
             homingState = HomingState.initHoming;
